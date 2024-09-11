fc816a5b10aad77633fa62eccc640bdac0c63e41
Aleksei Beliakov <Abel_1@mail.ru>
2024-09-11 16:36:27 +0300

    Шестое задание

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,58 @@
-﻿
+﻿"use strict"
+
+//Домашнее задание к лекции 6 «Обработка исключений и замыкания»
+//Задача 1. Форматтер чисел
+
+function parseCount(valueNeedParsed) {
+  if(isNaN(valueNeedParsed) || Number.parseFloat(valueNeedParsed) === NaN) {
+    throw new Error('Невалидное значение');
+  }
+  return Number.parseFloat(valueNeedParsed);
+}
+
+function validateCount(valueNeedParsed) {
+  try {
+    return parseCount(valueNeedParsed);
+  } catch(error) {
+      return error;
+  }
+}
+
+//Задача 2. Треугольник
+
+class Triangle {
+  constructor(a, b, c) {
+    this.a = a;
+    this.b = b;
+    this.c = c;
+
+    if(a + b < c || a + c < b || b + c < a) {
+      throw new Error('Треугольник с такими сторонами не существует');
+    }
+  }
+
+  get perimeter() {
+    return this.a + this.b + this.c;
+  }
+
+  get area() {
+    let p = this.perimeter / 2;
+    return +Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c)).toFixed(3);
+  }
+}
+
+function getTriangle(a, b, c) {
+  try {
+    let triangle = new Triangle(a, b, c);
+    return triangle;
+  } catch(error) {
+    return {
+      get perimeter() {
+        return 'Ошибка! Треугольник не существует';
+      },
+      get area() {
+        return 'Ошибка! Треугольник не существует';
+      }
+    }
+  }
+}
